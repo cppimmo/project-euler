91da4bd827fb6fdab99fc90197e2a55babf5d114
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-07-18 23:16:15 -0500

    Modify solution for problem 7

--- problem_007.clj
+++ problem_007.clj
@@ -1,4 +1,5 @@
-(ns day-007)
+(ns day-007
+  (:require [clojure.test :refer [deftest is testing]]))
 
 ;;;; Project Euler #3 - Largest Prime Factor
 
@@ -8,19 +9,27 @@
 
 ;;; Need a lazy seq of prime numbers
 
-(defn prime? [n]
-  (if (or (= n 2) (= n 3))
-    true
-    (and (pos? n)
-         (not (even? n))
-         ;; Check for divisibility by all integers less than or equal to sqrt(n)
-         (not-any? (fn [x]
-                     (zero? (mod n x)))
-                   (range 3 (-> n Math/sqrt int inc) 2)))))
+(declare prime? nth-prime primes)
 
-(def primes (filter prime? (iterate inc 2))) ; Probable primes lazy-sequence
+(deftest test-primes
+  (testing "..."
+    (is (= (take 7 primes) (list 2 3 5 7 11 13 17)))))
+
+(defn prime? [n]
+  (cond
+    (<= n 1) false
+    (=  n 2) true
+    :else (and (not (even? n))
+               ;; Check for divisibility by all integers less than or equal to sqrt(n)
+               (not-any? (fn [x]
+                           (zero? (mod n x)))
+                         (range 3 (-> n Math/sqrt Math/ceil inc) 2))))) ; [3, n)
+
+(def primes (filter prime? (iterate inc 2))) ; Probable primes lazy seq
 (defn nth-prime [n] ; Adjust since nth is zero-based
-  (nth primes (dec n)))
+  (if (<= n 0)
+    (throw (IllegalArgumentException.))
+    (nth primes (dec n))))
 
 (comment
   (let [n 10001]
